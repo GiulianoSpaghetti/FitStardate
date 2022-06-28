51975386ceed3ae1c4065eacfcfa865bf6986536
Giulio Sorrentino <49764967+numerunix@users.noreply.github.com>
2022-06-28 06:41:47 +0200

    Add files via upload

---
+++ index.jsx
@@ -0,0 +1,21 @@
+function mySettings(props) {
+  return (
+    <Page>
+      <Section>
+        <TextInput
+          label="Data gregoriana (aaa-mm-gg): "
+          title="gregoriandate"
+          settingsKey="gregoriandate"
+          type="text" 
+        />
+      <Button list label="Elimina impostazioni" onClick={() => props.settingsStorage.clear()} />
+      {<Text bold align="center">Creato da Giulio Sorrentino</Text>}
+      {<Text bold align="center">Dedicato a Francesca Milano</Text>}
+        {<Text bold align="center">Questo programma è sotto licenza GPL v3 o, secondo il tuo parere, qualsiasi versione successiva</Text>}
+      {<Text bold align="center">https://www.github.com/numerunix/FitStardate</Text>}
+     </Section>
+    </Page>
+  );
+}
+
+registerSettingsPage(mySettings);
