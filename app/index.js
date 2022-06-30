50a271837f127a44844dd1645175c95b624767c2
Giulio Sorrentino <49764967+numerunix@users.noreply.github.com>
2022-06-30 21:12:08 +0200

    Update index.js

--- app/index.js
+++ app/index.js
@@ -3,14 +3,14 @@ import { gettext } from "i18n";
 import * as messaging from "messaging";
 
   
-let gregoriandate = document.getElementById("gregoriandate");
-let  stardate = document.getElementById("stardate");
+let gregoriandate = document.getElementById('gregoriandate');
+let  stardate = document.getElementById('stardate');
 let mydata=null;
 let month=null;
 // Message is received
 messaging.peerSocket.onmessage = evt => {
   console.log(`App received: ${JSON.stringify(evt)}`);
-  if (evt.data.key === "gregoriandate" && evt.data.newValue) {
+  if (evt.data.key === 'gregoriandate' && evt.data.newValue) {
      mydata=JSON.parse(evt.data.newValue);
      mydata=new Date(mydata.name);
     gregoriandate.text=mydata.toDateString();
@@ -19,19 +19,19 @@ messaging.peerSocket.onmessage = evt => {
       month="0"+month.toString();
     else
       month=month.toString().substr(0,2);
-    stardate.text=mydata.getYear().toString()+month+"."+mydata.getDate().toString();
+    stardate.text=mydata.getYear().toString()+month+'.'+mydata.getDate().toString();
   }
 };
 
 // Message socket opens
 messaging.peerSocket.onopen = () => {
-  console.log("Copyright \u00A92022 Giulio Sorrentino<gsorre84@gmail.com>");
-  console.log("This progam is dedicated to Francesca Milano");
-  console.log("This program is distribuited under GPL. No Warranty is provided.");
-  console.log("App Socket Open");
+  console.log('Copyright \u00A92022 Giulio Sorrentino<gsorre84@gmail.com>');
+  console.log('This progam is dedicated to Francesca Milano');
+  console.log('This program is distribuited under GPL. No Warranty is provided.');
+  console.log('App Socket Open');
 };
 
 // Message socket closes
 messaging.peerSocket.onclose = () => {
-  console.log("App Socket Closed");
+  console.log('App Socket Closed');
 };
